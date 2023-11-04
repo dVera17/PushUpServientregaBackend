7a4126c6159e3f7c594c0a8c9df0e64c2a9eae70
dVera17 <dsvera174>
2023-11-03 20:01:49 -0500

    feat: :sparkles: Implement the endpoints created
    Implement the endpoints created

--- API/app.js
+++ API/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import "dotenv/config";
+import routerUser from './routes/user.routes.js';
+import routerEnvio from './routes/envio.routes.js';
 
 const app = express();
 app.set('hostname', process.env.HOSTNAME_SERVER);
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
+
+app.use('/user', routerUser);
+app.use('/envio', routerEnvio);
 
 export default app;
